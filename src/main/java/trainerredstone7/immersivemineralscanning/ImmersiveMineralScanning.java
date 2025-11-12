0fa083c5fbc533508193e6acfa87df2856bc7197
Trainerredstone7 <trainerredstone7@gmail.com>
2025-11-11 21:43:18 -0800

    Updated version number

--- src/main/java/trainerredstone7/immersivemineralscanning/ImmersiveMineralScanning.java
+++ src/main/java/trainerredstone7/immersivemineralscanning/ImmersiveMineralScanning.java
@@ -43,7 +43,7 @@ public class ImmersiveMineralScanning
 {
     public static final String MODID = "immersivemineralscanning";
     public static final String NAME = "Immersive Mineral Scanning";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public static Logger logger;
     @SidedProxy(clientSide = "trainerredstone7.immersivemineralscanning.proxy.ClientProxy", serverSide = "trainerredstone7.immersivemineralscanning.proxy.ServerProxy")
